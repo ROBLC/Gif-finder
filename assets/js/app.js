55edeb3466222648bf6448b23b5613d02d3bf0d3
RLC <robz.l.c.77@gmail.com>
2018-09-28 20:40:09 -0500

    gifs are now animating/pausing on click and  showing on search and on clicking on specific button for each search

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
 var searchButtons = ["jurassic Park", "Star Wars", "Indiana Jones", "Matrix"]
-
+var newsSearch;
+var still;
+var animate;
 //Show buttons with info from searchButtons 
 function renderButtons() {
     // Empties array so we dont repeat buttons 
@@ -10,6 +12,7 @@ function renderButtons() {
     }
 }
 function getGifs() {
+    $("#gif-area").empty();
     var search = $(this).attr("data-search");
     var GIFqueryURL = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=MNeVSWOTAJfR0WYeV1TbsDchBXgR4yhS&limit=10"
     console.log(search);
@@ -21,17 +24,41 @@ function getGifs() {
         console.log(response);
         var gifsArray = response.data
         for (var i = 0; i < gifsArray.length; i++) {
-            var columnDiv = $("<div>").addClass("col-sm-4");
-            var cardDiv = $("<div>").addClass("card").appendTo(columnDiv);
-            var gifImg = $("<img>").addClass("card-img-top").attr("src", gifsArray[i].images.fixed_height_small_still.url).appendTo(cardDiv);
+            var columnDiv = $("<div>").addClass("col-md-4 float-left");
+            var cardDiv = $("<div>").addClass("card mb-4").appendTo(columnDiv);
+            var gifImg = $("<img>").addClass("card-img-top GIF").attr("data-animate", gifsArray[i].images.fixed_height.url).attr("data-still", gifsArray[i].images.fixed_height_still.url).attr("src", gifsArray[i].images.fixed_height_still.url).attr("data-state", "still").appendTo(cardDiv);
+            gifImg.attr("height", "200px").attr("width", "200px")
             var cardBody = $("<div>").addClass("card-body").appendTo(cardDiv);
-            var cardText = $("<div>").addClass("card-text").appendTo(cardDiv);
-            var title = $("<p>").text("Title: " + gifsArray[i].title).addClass("text-center").appendTo(cardDiv);
-            var rating = $("<p>").text("Rating: " + gifsArray[i].rating).addClass("text-center").appendTo(cardDiv);
+            var cardText = $("<div>").addClass("card-text").appendTo(cardBody);
+            var title = $("<p>").text("Title: " + gifsArray[i].title).addClass("mb-1").appendTo(cardText);
+            var rating = $("<p>").text("Rating: " + gifsArray[i].rating).addClass("mb-1").appendTo(cardText);
+            $(columnDiv).prependTo($("#gif-area"))
+        }
+    });
+}
 
+function getGifs2() {
+    $("#gif-area").empty();
+    var search = newSearch;
+    var GIFqueryURL = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=MNeVSWOTAJfR0WYeV1TbsDchBXgR4yhS&limit=10"
+    console.log(search);
 
-            $(columnDiv).prependTo($(".jumbotron"))
-
+    $.ajax({
+        url: GIFqueryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        var gifsArray = response.data
+        for (var i = 0; i < gifsArray.length; i++) {
+            var columnDiv = $("<div>").addClass("col-md-4 float-left");
+            var cardDiv = $("<div>").addClass("card mb-4").appendTo(columnDiv);
+            var gifImg = $("<img>").addClass("card-img-top GIF").attr("data-animate", gifsArray[i].images.fixed_height.url).attr("data-still", gifsArray[i].images.fixed_height_still.url).attr("src", gifsArray[i].images.fixed_height_still.url).attr("data-state", "still").appendTo(cardDiv);
+            gifImg.attr("height", "200px").attr("width", "200px")
+            var cardBody = $("<div>").addClass("card-body").appendTo(cardDiv);
+            var cardText = $("<div>").addClass("card-text").appendTo(cardBody);
+            var title = $("<p>").text("Title: " + gifsArray[i].title).addClass("mb-1").appendTo(cardText);
+            var rating = $("<p>").text("Rating: " + gifsArray[i].rating).addClass("mb-1").appendTo(cardText);
+            $(columnDiv).prependTo($("#gif-area"))
         }
     });
 }
@@ -39,7 +66,7 @@ function getGifs() {
 $("#addSearch").on("click", function (event) {
     event.preventDefault();
 
-    var newSearch = $("#searchInput").val().trim();
+    newSearch = $("#searchInput").val().trim();
     //if statement to check if a search is already in array 
     if (searchButtons.includes(newSearch)) {
         console.log("already in array")
@@ -51,10 +78,24 @@ $("#addSearch").on("click", function (event) {
     renderButtons();
     //reset the form each time a search is added
     $("#searchForm")[0].reset();
-    getGifs();
+    getGifs2();
 });
-$(document).on("click", ".searchItem", getGifs)
-
-
 renderButtons();
+
+$(document).on("click", ".searchItem", getGifs)
+$(document).on("click", ".GIF", function () {
+    console.log(this);
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    }
+    else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+});
+
+
+
 
